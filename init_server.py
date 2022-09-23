fddf60d4df68eac90516cff203a5152147cce532
Luis Emilio Grijalba <luisegrijalba8@gmail.com>
2022-09-23 18:29:07 -0500

    union of automatons with Flask

--- init_server.py
+++ init_server.py
@@ -1,26 +1,39 @@
 from flask import Flask, request
 from model.Automaton import Automaton
 from controller.Automaton_operation import Operations
+import json
 
 
 app = Flask(__name__)
 
 
-@app.route('/union/', methods=['GET'])
+@app.route('/union', methods=['GET'])
 def getUnio():
-    quintuples = request.json
-    quintuple1 = quintuples["quintuple1"]
-    quintuple2 = quintuples["quintuple2"]
+    quintuple_one = None
+    quintuple_two = None
+
+    try:
+        quintuple_one = request.files.get('quintuple_one')
+        quintuple_one = json.load(quintuple_one)
+
+        quintuple_two = request.files.get('quintuple_two')
+        quintuple_two = json.load(quintuple_two)
+    except:
+        return {"message": "Archivo(s) no válido(s)"}, 400
 
     a1 = Automaton()
-    a1.load_automaton_dict(quintuple1)
     a2 = Automaton()
-    a2.load_automaton_dict(quintuple2)
+
+    try:
+        a1.load_automaton_dict(quintuple_one)
+        a2.load_automaton_dict(quintuple_two)
+    except Exception as error:
+        return {"message": str(error)}, 400
 
     union = Operations.automaton_union(a1, a2)
     unio_quintuple = union.get_quintuple()
 
-    return {"automaton_union": unio_quintuple}
+    return {"message": "operación exitosa.", "automaton_union": unio_quintuple}, 400
 
 
 @app.route('/address', methods=["POST"])
